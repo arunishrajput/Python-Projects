76864ee51059e4ad117fae5087e7cd51e5ff8518
Arunish Rajput <105339744+arunishrajput@users.noreply.github.com>
2022-10-22 13:28:19 +0530

    Add files via upload

---
+++ Whatsapp message schedular.py
@@ -0,0 +1,5 @@
+#Whatsapp message schedular
+import pywhatkit #install 'pywhatkit' by typing 'pip install pywhatkit' in Terminal
+pywhatkit.sendwhatmsg('+91number you want to send this message to', 'message you want to send', hour time, minute time)#enter your corrosponding details
+
+#make sure that your web browser should linked to 'web.whatsapp.com'
