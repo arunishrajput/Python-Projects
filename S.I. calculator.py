2331ca40b8510a2af2564b0e7273b78013924919
Arunish Rajput <105339744+arunishrajput@users.noreply.github.com>
2024-06-28 10:06:01 +0530

    remove some files

--- S.I. calculator.py
+++
@@ -1,5 +0,0 @@
-#S.I. calculator
-P = int (input('Principal amount:')) # It will take input as Principal amount:
-T = int (input('Time period:')) # It will take input as Time period:
-R = int (input('Rate:')) # It will take input as Rate:
-print ('S.I =',P*T*R/100) # This is the final result
